965feb20257ba8e518c1b046002d16381fbea233
Alexwoko <36886799+Alexwoko@users.noreply.github.com>
2020-10-26 12:48:20 +0100

    Add files via upload

---
+++ PathRequestManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+//------------------------------------ || Request Manager class ||-----------------------------------\\
+
+
+// This class is controlling the path requests from units by making a queue system --> This is to avoid that more than one enemy calls for a path in the same frame
+
+
+public class PathRequestManager : MonoBehaviour {
+    
+
+	public bool useHeap;
+    public bool useLoopSearch;
+
+	Queue<PathRequest> pathRequestQueue = new Queue<PathRequest> ();                    // This is request queue --> firast in - first out.
+	PathRequest currentPathRequest;                                                     // Our current path request.  
+
+
+
+    static PathRequestManager instance;                                                 // The pathRequestManager that is being used now. (this) --> self-reference (an instance of the script)                                             
+	Pathfinding pathfinding;                                                            // A pathfinding object 
+
+	bool isProcessingPath;                                                              // Boolean to know if the request is alreeady in queue
+
+    void Awake(){   // Awake is called when the script object is initialized. Is called once in the lifetime of the script.
+                    // Is called on all objects in the scene before any objects "Start" method --> unless objects are instantiated during gameplay.
+
+
+		instance = this;                                                                // Self-reference --> instance = this instance..
+		pathfinding = GetComponent<Pathfinding> ();                                     // Is placed in awake so that this will be the first thing to happen --> we instanciate the pathfinding variable as a pathfinding object.
+
+	}
+
+    //------------------------------------ || Request a path ||-----------------------------------\\
+
+	public static void RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callback){       
+
+		PathRequest newRequest = new PathRequest (pathStart, pathEnd, callback);         // A new path Request taking in a start and end position and a callback to tell if a path is found.
+		instance.pathRequestQueue.Enqueue (newRequest);                                  // Put the new path request into the queue
+		instance.TryProcessNext ();                                                      // Check if we have a path yet                                                                                              
+
+	
+	}
+
+    //------------------------------------ || Begin processing a path ||-----------------------------------\\
+
+	void TryProcessNext(){
+
+		if (!isProcessingPath && pathRequestQueue.Count > 0) {
+            // If the path is not already processing and the queue is not empty
+
+			currentPathRequest = pathRequestQueue.Dequeue ();                            // Take the path request out of the Queue
+			isProcessingPath = true;                                                     // We are now processing the path
+
+
+			if (useHeap){
+				useLoopSearch = false;
+			
+			pathfinding.StartFindPathHeap (currentPathRequest.pathStart, currentPathRequest.pathEnd);       // Ask the pathfinding object to find a path
+			} else if (useLoopSearch){
+				useHeap = false;
+				pathfinding.StartFindPathLoop(currentPathRequest.pathStart, currentPathRequest.pathEnd);
+
+			}
+			}
+
+	}
+
+    //------------------------------------ || finished processing the path ||-----------------------------------\\
+
+	public void FinishedProcessingPath(Vector3[] path, bool success){
+
+		currentPathRequest.callback (path, success);                    // Insert the path and the succes criteria into the callback Action
+		isProcessingPath = false;                                       // We are not processing the path anymore 
+		TryProcessNext();                                               // Check if a new path request has entered the queue
+
+
+	}
+
+    //------------------------------------ || The path request structure ("class") ||-----------------------------------\\
+
+	struct PathRequest{
+
+		public Vector3 pathStart;
+		public Vector3 pathEnd;
+		public Action<Vector3[], bool> callback;
+
+        //------------------------------------ || Path request constructor ||-----------------------------------\\
+
+		public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback){
+
+			pathStart = _start;
+			pathEnd = _end;
+			callback = _callback;
+
+
+
+		}
+	}
+}
---
+++ Unit.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using FSM;                  // Finite state machine --> Own collection
+
+//------------------------------------ || Unit ||-----------------------------------\\
+
+public class Unit : MonoBehaviour {    // monobehavior == base class. (Like the object-class in java?)
+
+
+
+   // int detectionRange = 40;
+   // bool closeEnough = false;
+
+
+
+
+
+    // New unnit  class variables --> (For the statemachine):
+
+                                                          // A counter to keep track of seconds in the gameTimer
+
+   // public StateMachine<Unit> stateMachine { set; get; }                            // A statemachine variable
+
+
+
+
+
+    // Original unit class variables --> (for the pathfinding):
+
+	public Transform target;    // Tranform can access: position, rotation and scale. We use it for the position of the target.
+	public float speed;           // Unit speed
+	Vector3[] path;             // Array of Vector3 (worldpoints) that holds the path.
+    int targetIndex;            // Moves the unit closer to the target (Has a value for everytime we change directions --> targetIndex increments when we change directions)
+
+	void Start(){   // enabled before any updates are called for the first time. Is called once in the lifetime of a script
+
+
+        // new methods for the state machine:
+
+       // stateMachine = new StateMachine<Unit>(this);                                // A statemachine object 
+      //  stateMachine.ChangeState(IdleState.Instance);                               // Set tghe state to be the idle state (First state)
+      //  gameTimer = Time.time;                                                      // game timer object
+
+
+        // Original method for pathfinding:
+
+
+
+             PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);   // Request a path from our path request manager
+        
+
+	}
+
+    //------------------------------------ || Path found! ||-----------------------------------\\
+
+	public void OnPathFound(Vector3[] newPath, bool pathSuccessful){               // We have found the shortest path.
+
+		if (pathSuccessful) {                                                      // If we have found the shortest path
+
+			path = newPath;                                                        // The path is our new path
+			StopCoroutine ("FollowPath");                                          // Stop the coroutine --> if there is already a coroutine going on
+			StartCoroutine ("FollowPath");                                         // start the coroutine 
+
+            // A coroutine is a function that can pause the execution of a method and leave the control back to unity.
+            // We use this so that we don´t run all the code of a method to end at once in a single frame --> 
+
+		}
+
+	}
+
+    //------------------------------------ || Follow the path ||-----------------------------------\\
+
+    // IEnumerator is used when we use Coroutine
+    IEnumerator FollowPath()
+    {                                                   // We have found a path, now follow it!
+
+        Vector3 currentWaypoint = path[0];                                     // Vector3 that is a current waypoint from the simplified path
+        {
+            while (true)
+            {                                                          // Runs until we break or return
+                if (transform.position == currentWaypoint)
+                {
+                    // When we hit a waypoint in the simplified path
+
+                    targetIndex++;                                                  // Increments target index
+                    if (targetIndex >= path.Length)
+                    {
+                        // If we have moved past or are equal to the target
+                        yield break;                                                // break the while loop
+
+                    }
+                    currentWaypoint = path[targetIndex];                            // The current waypoint is the (next/ incremented) incrementet targetIndex waypoint
+                }
+
+                transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+                // Update the position --> move the unit closer to target.
+
+
+                yield return null;                                                  // return nothing
+            }
+
+        }
+    }
+
+	// New method for the state machine:
+
+
+    /*
+	private void Update()
+	{
+
+
+
+
+        if (Vector3.Distance(target.position, transform.position) <= detectionRange){
+            closeEnough = true;
+            PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+            Debug.Log("We´re in soldiers!");
+        }
+        */
+
+        /*
+        if(Time.time > gameTimer + 1){                                          
+            // if time has past
+
+            gameTimer = Time.time;                                              // Set our game timer to unitys timer --> now we can keep track of the time with our seconds variable
+            seconds++;                                                          // increment seconds by one
+            Debug.Log(seconds);                                                 // DEBUG 
+
+
+        }
+        if (seconds == 5){                                                      // If five seconds have past
+            seconds = 0;                                                        // re-set seconds back 0
+            switchState = !switchState;                                         // Switching state to opposite of current state                          
+            chaseState = !chaseState;
+            //idleState = false;
+        }
+        stateMachine.Update();                                                  // Update the state machine 
+      
+        
+	}
+  */
+
+	//------------------------------------ || Graphical indicators ||-----------------------------------\\
+
+	public void OnDrawGizmos(){
+
+		if (path != null) {
+			for (int i = targetIndex; i < path.Length; i++) {
+				Gizmos.color = Color.black;
+				Gizmos.DrawCube (path [i], Vector3.one);
+
+				if (i == targetIndex) {
+					Gizmos.DrawLine (transform.position, path [i]);
+
+				} else {
+
+					Gizmos.DrawLine (path[i-1], path[i]);
+				}
+
+			}
+
+
+		}
+	}
+
+}
